47308eed1bf66e3eda09d7c35b738afaeaa7ea5b
Daniil Khokhulya <khokhulya2006@gmail.com>
2024-12-21 15:56:04 +0100

    This commit creates and configures dbContext and connection string for MSSQL

---
+++ BdCourseWork/Context/AppDbContext.cs
@@ -0,0 +1,51 @@
+﻿using BdCourseWork.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BdCourseWork.Context 
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Worker> Workers;
+
+        DbSet<Morgue> Morgues;
+
+        DbSet<MorgueWorker> MorguesWorkers;
+
+        DbSet<CadaverRoom> CadaverRooms;
+
+        DbSet<Cadaver> Cadavers;
+
+        DbSet<FunerealAgency> FunerealAgencies;
+
+        DbSet<FunerealAgencyWorker> FunerealAgenciesWorkers;
+
+        DbSet<Ritual> Rituals;
+
+        DbSet<Ashes> Ashes;
+
+        DbSet<Urn> Urnes;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+          
+        }
+
+        public AppDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("Course_Work");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
--- BdCourseWork/Program.cs
+++ BdCourseWork/Program.cs
@@ -1,3 +1,6 @@
+using BdCourseWork.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace BdCourseWork
 {
     public class Program
@@ -8,6 +11,9 @@ namespace BdCourseWork
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Course_Work")));
 
             var app = builder.Build();
 
